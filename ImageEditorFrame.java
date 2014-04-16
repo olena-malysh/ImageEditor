377a0d951b3c2d1f2be9d986f56bb64d9ec36141
Olena Malyshko <olena.malyshko@haw-hamburg.de>
2014-04-16 15:33:41 +0200

    leeres Frame initialisiert

---
+++ ImageEditorFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class ImageEditorFrame extends JFrame{ 
+	public ImageEditorFrame(){ 
+		setDefaultCloseOperation(EXIT_ON_CLOSE); 
+		setSize(400, 300); 
+		setVisible(true); 
+	} 
+	public static void main(String[] args){
+		ImageEditorFrame frame = new ImageEditorFrame();
+	}
+}
